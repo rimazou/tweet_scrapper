91082731d2272a95072f736ded5c445957139eae
Rima Zourane <56721441+rimazou@users.noreply.github.com>
2022-11-08 17:21:56 +0100

    update manager.py

--- manager.py
+++ manager.py
@@ -1,11 +1,11 @@
 #ce fichier sera utilise pour y mettre les differentes cles et constantes pour l'instant
 #ACCESS KEYS TWITTER API
-consumer_key = 'dZbG53gSRASfPictPHGWMwNBW'
-consumer_secret = 'w6UVNwH7oNChDLyGGdRebRpV1dxY9qv5eevsN6rwFt5VdSFSYh'
-bearer_token = 'AAAAAAAAAAAAAAAAAAAAAM5uTwEAAAAAsFdMGFd%2FJE29VMAMmjdshJzr5KU%3DiRxoBSEClIcECmGewsUQUbPc58E83Px10qMUtO71svbARXb86a' 
+consumer_key = ''
+consumer_secret = ''
+bearer_token = '' 
 path = "../datasets/ "
-access_token = '1304891973959135239-x2hbArreelQQVFSc9FpkmKOvnvto7v'
-access_token_secret = 'oSYpVR55zc7xZNnApZAPlT099qdO7vbqTTglT76p8YEr2'
+access_token = ''
+access_token_secret = ''
 MAX_RECENT= 1000 # we can only retrieve 18000 of tweets within 15min
 MAX_USER=3200 # we can only retrieve this amount of recent tweets of a user timeline
 conv_id=1445811612775030785 # will be used for the tests onlyn   
