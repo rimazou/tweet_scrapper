c8b5af0f35d2fe1569479404c37cd9d0b983f2be
Rima Zourane <56721441+rimazou@users.noreply.github.com>
2022-11-08 17:22:27 +0100

    delete tuto.py

--- tuto.py
+++
@@ -1,8 +0,0 @@
-# Import all needed libraries
-import tweepy                   # Python wrapper around Twitter API
-from google.colab import drive  # to mount Drive to Colab notebook
-import json
-import csv
-from datetime import date
-from datetime import datetime
-import time
